4671f1ac07eab47bea927314abcf3859fa75762b
Jonas Holbech <jh@jonasholbech.dk>
2018-08-23 09:12:47 +0200

    commit number x+=3

--- main.js
+++ main.js
@@ -12,6 +12,21 @@
       c.setAttribute("cy", Math.random() * window.innerHeight);
       app.appendChild(c);
     }
+
+    let circles = document.querySelectorAll("circle");
+    console.log(circles);
+    circles.forEach(c => {
+      c.addEventListener("mouseenter", e => {
+        //app.appendChild(e.target);
+        e.target.style.fill = `hsl(${Math.random() * 360},${70}%,${70}%)`;
+
+        let cx = parseInt(e.target.getAttribute("cx"));
+        let cy = parseInt(e.target.getAttribute("cy"));
+        let newX = Math.random() * window.innerWidth - cx;
+        let newY = Math.random() * window.innerHeight - cy;
+      });
+    });
   }
   window.addEventListener("DOMContentLoaded", makeCircles);
+  window.addEventListener("resize", makeCircles);
 })();
